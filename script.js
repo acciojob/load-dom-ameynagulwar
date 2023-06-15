b05e13d6ef482302c03ea48ee8eb165b76feb3ff
Amey <130839577+ameynagulwar@users.noreply.github.com>
2023-06-15 16:35:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+document.addEventListener("DOMContentLoaded", function() {
+  // Clear the body tag
+  document.body.innerHTML = "";
+
+  // Create a new text node
+  var textNode = document.createTextNode("DOM load success");
+
+  // Append the text node to the body tag
+  document.body.appendChild(textNode);
+});
